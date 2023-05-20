b42c3571467f3dfd77b64530abb7dc3d9284ae58
kuttu101996 <kuttulr1996@gmail.com>
2023-05-20 13:18:27 +0530

    v app day2 1st commit

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
-const express = require("express");
-const { Server } = require("socket.io")
-const http = require("http")
+// server.js
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 
-const app = express()
-const httServer = http.createServer(app)
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-const io = new Server(httServer)
+app.use(express.static(__dirname + '/public'));
 
+// Store the connected users' streams
+const connectedUsers = {};
 
-httServer.listen(9999, ()=>{
-    console.log("server at 9999")
-})
+io.on('connection', socket => {
+  // Listen for 'user-connected' events from clients
+  socket.on('user-connected', stream => {
+    connectedUsers[socket.id] = stream;
+
+    // Emit the 'user-connected' event to all other connected clients, passing the ID and stream of the new user
+    socket.broadcast.emit('user-connected', { userId: socket.id, stream });
+
+    // Listen for 'disconnect' events from clients
+    socket.on('disconnect', () => {
+      // Delete the disconnected user's stream from the connected users' object
+      delete connectedUsers[socket.id];
+
+      // Emit the 'user-disconnected' event to all connected clients, passing the ID of the disconnected user
+      socket.broadcast.emit('user-disconnected', socket.id);
+    });
+
+    // Listen for 'call-ended' events from clients
+    socket.on('call-ended', () => {
+      // Emit the 'call-ended' event to all connected clients
+      io.emit('call-ended');
+    });
+  });
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
+});
